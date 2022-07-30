a9f33df91833944df4206035faa34e2faabeac11
pramil-paudel <ppaudel35@gmail.com>
2022-07-30 15:12:46 -0500

    gdownload_updated

--- download_googledrive.sh
+++ download_googledrive.sh
@@ -1,1 +1,11 @@
-python3 google_drive_downloader.py
+#!/bin/bash
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres="gpu:titanxp:1"
+#SBATCH -c 4
+#SBATCH --mem=32G
+#SBATCH --time=48:00:00
+#SBATCH -J train_test_separator
+#SBATCH -o slurm-%j.out
+
+python3 drive_downloader.py
--- drive_downloader.py
+++ drive_downloader.py
@@ -1,8 +1,5 @@
-from zdrive import Downloader
+import gdown
 
-output_directory = "/Users/patthar/Downloads"
-d = Downloader()
-
-# folder which want to download from Drive
-folder_id = '1Uzpf8L72q45XoO-jdw96B_qsWkz2ICNP'
-d.downloadFolder(folder_id, destinationFolder=output_directory)
+url = 'https://drive.google.com/uc?id=1Uzpf8L72q45XoO-jdw96B_qsWkz2ICNP'
+output = 'face_data_download.zip'
+gdown.download(url, output, quiet=False)
