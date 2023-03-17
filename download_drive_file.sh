c3fa014207a5d2879979c38a974e7e1333853b7c
pramil-paudel <ppaudel35@gmail.com>
2023-03-17 04:44:43 -0500

    Updated image creating system.

--- download_drive_file.sh
+++ download_drive_file.sh
@@ -7,4 +7,4 @@
 #SBATCH --time=48:00:00
 #SBATCH -J DATA_DOWNLOAD
 #SBATCH -o slurm-%j.out
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=19vdaXVRtkP-nyxz1MYwXiFsh_m_OL72b' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=19vdaXVRtkP-nyxz1MYwXiFsh_m_OL72b" -O utk_part_2.tar.gz  && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oj9ZWsLV2-k2idoW_nRSrLQLUP3hus3b' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1oj9ZWsLV2-k2idoW_nRSrLQLUP3hus3b" -O utk_part_3.tar.gz  && rm -rf /tmp/cookies.txt
